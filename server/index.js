edf90995ad11dc6733c8c9399422cb6fc24bbfac
Dinara Bekeshova <dbekeshova@gmail.com>
2023-11-13 09:18:42 +0100

    chore: enable cors

--- server/index.js
+++ server/index.js
@@ -3,9 +3,14 @@
 const express = require('express');
 const app = express();
 const router = require('./router');
+var cors = require('cors');
+
 
 const PORT = 3000;
 const HOST = 'localhost';
+
+// enable cors
+app.use(cors());
 
 // handle incoming JSON data in the request body
 app.use(express.json());
