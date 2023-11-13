550e0be49f8a35d0a8a2f362864b50dd1b892d61
Dinara Bekeshova <dbekeshova@gmail.com>
2023-11-13 11:41:00 +0100

    chore: refactor seedDB

---
+++ server/utils/seedMovementsDB.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+require('../db');
+const fs = require('fs');
+const Movement = require('../models/movement');
+
+fs.readFile('./sampleMovements.json', 'utf8', (err, data) => {
+  console.log(data);
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  const jsonData = JSON.parse(data);
+
+  Movement.insertMany(jsonData)
+    .then(() => {
+      console.log('Movements seeded successfully✨');
+      mongoose.connection.close();
+    })
+    .catch((error) => {
+      console.error('Error seeding Movements:', error);
+      mongoose.connection.close();
+    });
+});
---
+++ server/utils/seedWorkoutsDB.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+require('../db');
+const fs = require('fs');
+const Workout = require('../models/workout');
+
+fs.readFile('./samplewods.json', 'utf8', (err, data) => {
+  console.log(data);
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  const jsonData = JSON.parse(data);
+
+  Workout.insertMany(jsonData)
+    .then(() => {
+      console.log('Workouts seeded successfully✨');
+      mongoose.connection.close();
+    })
+    .catch((error) => {
+      console.error('Error seeding Workouts:', error);
+      mongoose.connection.close();
+    });
+});
