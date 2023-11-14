abf0ae5a28f73d48927996395a7080060c5456e3
Dinara Bekeshova <dbekeshova@gmail.com>
2023-11-14 23:43:44 +0100

    chore: refactor

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # eatliftlove
+
+### How to run the app
 - Desktop version only for now
 - to start server run `npm install` then `nodemon index.js`
 - to start client run `npm install` then `npm run dev`
+
--- client/src/pages/Findagym.jsx
+++ client/src/pages/Findagym.jsx
@@ -5,7 +5,9 @@ const Findagym = () => {
 		<div className="text-center text-white p-5">
 			<h1 className="text-3xl font-bold ">Find a Crossfit box nearby</h1>
 			<br />
-			<p>✨ Crossfit gyms (aka box) in a beautiful city of Berlin ✨</p>
+			<p>
+				✨ Recommended Crossfit gyms (aka box) in a beautiful city of Berlin ✨
+			</p>
 			<div className="flex justify-center">
 				<GoogleMap />
 			</div>
